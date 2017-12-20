326b79d3be43041489bb9b83a4b425789218f1cf
Bojan Antolović <bojan.antolovic@gmail.com>
2017-12-20 10:31:32 +0100

    Release 2.1.0.

--- Build.bat
+++ Build.bat
@@ -1,6 +1,6 @@
 SETLOCAL
-SET Version=2.0.1
-SET Prerelease=auto
+SET Version=2.1.0
+SET Prerelease=
 
 IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
 @ECHO ON
